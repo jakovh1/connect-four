443c1580ebab6b698acfc6890a3622bd6d557110
Jakov <1jakov.1horvat@gmail.com>
2024-12-01 02:48:46 +0100

    #make_move method and initial passing test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,6 +5,14 @@ require_relative '../lib/cage'
 
 describe Player do
   describe '#make_move' do
+    it 'Inserts a red piece and returns the position where it is inserted.' do
+      cage = Cage.new
+      player_red = Player.new('red')
+      player_yellow = Player.new('yellow')
+
+      allow($stdin).to receive(:gets).and_return('1')
+      expect(player_red.make_move(cage)).to eql([0, 5, player_red.symbol])
+    end
     
   end
 end
