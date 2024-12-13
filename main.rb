346b3e19a709f208f6c88ba26c035a48b1c1d9bb
Jakov <1jakov.1horvat@gmail.com>
2024-12-13 23:44:57 +0100

    Game class and integration test

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'lib/cage'
-require_relative 'lib/player'
+require_relative 'lib/game'
 
-cage = Cage.new
-cage.print_cage
+game = Game.new
+game.play
+puts
